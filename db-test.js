7ddc600bbffdfc585e418aa4631354b897fee084
Jose Pagan <josepagan@pop-os.localdomain>
2022-06-08 20:37:27 +0100

    added info about installation of sqlite3

--- db-test.js
+++ db-test.js
@@ -1,6 +1,10 @@
 const sqlite = require('sqlite');
 
 async function setup() {
+
+//needs to install sqlite3 and add it as driver of squlite as explained at... 
+// https://stackoverflow.com/questions/63823970/sqlite-filename-cannot-be-null-undefined
+
     const db = await sqlite.open('./mydb.sqlite')
     // await db.migrate({force:'last'})
     // const users = await db.all('SELECT * FROM User')
