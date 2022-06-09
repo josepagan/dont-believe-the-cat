a1a07753754309eaacdf8c3f4a64c1656ea9ef01
Jose Pagan <josepagan@pop-os.localdomain>
2022-06-09 11:23:50 +0100

    feat:sqlite3 installed

--- db-test.js
+++ db-test.js
@@ -1,3 +1,4 @@
+const sqlite3 = require('sqlite3');
 const sqlite = require('sqlite');
 
 async function setup() {
@@ -5,8 +6,8 @@ async function setup() {
 //needs to install sqlite3 and add it as driver of squlite as explained at... 
 // https://stackoverflow.com/questions/63823970/sqlite-filename-cannot-be-null-undefined
 
-    const db = await sqlite.open('./mydb.sqlite')
-    // await db.migrate({force:'last'})
+    const db = await sqlite.open({filename: './mydb.sqlite', driver: sqlite3.Database})
+    await db.migrate({force:'last'})
     // const users = await db.all('SELECT * FROM User')
     // console.log('ALL PEOPLE', JSON.stringify(users, null, 2))
 }
--- migrations/001-helloworld.sql
+++ migrations/001-helloworld.sql
@@ -1,32 +1,32 @@
 -- Up
 CREATE TABLE User (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(50) NOT NULL,
+    name TEXT NOT NULL,
     email TEXT  NOT NULL UNIQUE,
     homeId INTEGER KEY REFERENCES Home(id)
 
 );
 
-CREATE TABLE Home (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+-- CREATE TABLE Home (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
     
-);
+-- );
 
-CREATE TABLE Cat (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    homeId INTEGER KEY REFERENCES Home(id)
-);
+-- CREATE TABLE Cat (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     name TEXT,
+--     homeId INTEGER KEY REFERENCES Home(id)
+-- );
 
--- INSERT INTO User(name,email) VALUES ('Jose','jm.pagan@omg.com')
-
-
+-- -- INSERT INTO User(name,email) VALUES ('Jose','jm.pagan@omg.com')
 
 
 
 
---Down
-DROP TABLE User;
-DROP TABLE Home;
-DROP TABLE Cat;
+
+
+-- --Down
+-- DROP TABLE User;
+-- DROP TABLE Home;
+-- DROP TABLE Cat;
 
